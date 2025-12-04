6cb6723a1737f8179a088b2e4f2dccf8fc9917a1
isaaczhu <isaaczhu@gmail.com>
2025-12-04 17:14:37 -0500

    naming clashes in NEWKARU

--- new_stuff/NEWKARU/tests/test_sparta_single.py
+++ new_stuff/NEWKARU/tests/test_sparta_single.py
@@ -1,5 +1,11 @@
 from __future__ import annotations
 
+# add ./new_stuff/NEWKARU to sys.path for imports
+import os
+import sys
+path_string = "/Users/isaaczhu/MIT/25-26/HIKARU/new_stuff/NEWKARU"
+sys.path.append(os.path.abspath(path_string))
+
 from blueprints.heuristic_blueprint import HeuristicBlueprint
 from envs.full_hanabi_env import FullHanabiEnv
 from search.sparta_single import SpartaSingleAgentWrapper, SpartaConfig
