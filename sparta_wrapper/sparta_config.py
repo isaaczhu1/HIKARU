61bc2089063b53b8b59bd1957541a7f0837ca7f3
albertwang2004 <albert.wang.2004@gmail.com>
2025-12-10 06:22:54 +0000

    partial sparta impl

--- sparta_wrapper/sparta_config.py
+++ sparta_wrapper/sparta_config.py
@@ -7,4 +7,16 @@ HANABI_GAME_CONFIG = {
     "max_life_tokens": 3,
 }
 
+DEVICE = "cuda"
+
 CKPT_PATH = "runs/hanabi/standard_train/20251210_000611/ckpt_001000.pt"
+
+SPARTA_CONFIG = {
+    "num_rollouts": 16,
+    "epsilon": 0.05,
+    "rng_seed": None,
+
+    # for rejection sampling
+    "upstream_factor": 5,
+    "max_attempts": 100,
+}
--- sparta_wrapper/tests/albert_tests.py
+++ sparta_wrapper/tests/albert_tests.py
@@ -10,11 +10,12 @@ import random
 
 rng = random.Random(42)
 
+from hanabi_gru_baseline.config import CFG as GRU_CFG
 from sparta_wrapper.belief_models import sample_world_state, _iter_all_hands, _compute_remaining_deck, _predict_partner, _hand_multiplicity, _sample_hand, _iter_all_hands
 from sparta_wrapper.hanabi_utils import build_observation, HanabiLookback1, unmask_card, fabricate
 from sparta_wrapper.hanabi_utils import HanabiObservation, _advance_chance_events, build_observation
 
-from sparta_wrapper.gru_blueprint import CkptGuardFactoryFactory
+from sparta_wrapper.gru_blueprint import SamplerGRUFactoryFactory
 from sparta_wrapper.sparta_config import HANABI_GAME_CONFIG, CKPT_PATH
 
 class SignalBlueprint:
@@ -137,7 +138,7 @@ def squid_game(seed):
         lagging_state=lookback1.prev_state,
         obs=obs1,
         rng=rng,
-        blueprint_factory=CkptGuardFactoryFactory(CKPT_PATH),
+        blueprint_factory=SamplerGRUFactoryFactory(GRU_CFG, CKPT_PATH),
         takes=50,
         upstream_factor=5,
         max_attempts=3,
@@ -159,9 +160,7 @@ def squid_game(seed):
             print(move.move().type())
 
     fabricated_move_history = fabricate(lookback1.cur_state, 1, [(0, 2), (0, 2), (0, 0)])
-
-    for move in fabricated_move_history:
-        print(move, move.player(), move.color(), move.rank())
+    print(fabricated_move_history)
 
 def octopus_activities(seed):
     game = pyhanabi.HanabiGame(HANABI_GAME_CONFIG)
